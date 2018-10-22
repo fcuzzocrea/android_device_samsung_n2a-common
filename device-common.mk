2dc15afec11a73766d553a632041bd7fe31ad63e
Stricted <info@stricted.net>
2018-10-22 06:04:08 +0200

    n2a: set PRODUCT_AAPT_CONFIG
    Change-Id: Id12f387282c28915bbc7d2d0da64fe9abd36f20f

--- device-common.mk
+++ device-common.mk
@@ -21,6 +21,12 @@ DEVICE_PATH := device/samsung/n2a-common
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
 
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := xlarge
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+# A list of dpis to select prebuilt apk, in precedence order.
+PRODUCT_AAPT_PREBUILT_DPI := hdpi mdpi
+
 # AdvancedDisplay (MDNIE)
 PRODUCT_PACKAGES += \
     AdvancedDisplay
