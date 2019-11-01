8dacf2e9cb30f0c1754dcbd0a3646f06fa40375e
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-01 07:19:07 -0500

    n2a: Exclude lineage-sdk overlays from RRO
    * lineage-sdk overlays don't work with RRO
    
    Change-Id: Iff826faf1864388066a488acc9682636b98d1098

--- device-common.mk
+++ device-common.mk
@@ -20,6 +20,8 @@ DEVICE_PATH := device/samsung/n2a-common
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    device/samsung/n2a-common/overlay/lineage-sdk
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := xlarge
