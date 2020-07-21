ad96c4fb60d8e1b9aed7eeeb420fa1dd55adccb7
Francescodario Cuzzocrea <bosconovic@gmail.com>
2020-07-21 15:54:21 +0000

    n2a: Exclude overlay/hardware/samsung/AdvancedDisplay from RRO
    Change-Id: Id612d051188859586c0ca15c692ef7b9ffcb1003

--- device-common.mk
+++ device-common.mk
@@ -21,7 +21,8 @@ DEVICE_PATH := device/samsung/n2a-common
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    device/samsung/n2a-common/overlay/lineage-sdk
+    device/samsung/n2a-common/overlay/lineage-sdk \
+    device/samsung/n2a-common/overlay/hardware/samsung/AdvancedDisplay
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := xlarge
